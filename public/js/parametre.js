738d1781be327cb18920eb2c5adadd446f7682a3
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2021-04-10 18:46:25 +0200

    Ajout du lock pointer

--- public/js/parametre.js
+++ public/js/parametre.js
@@ -6,11 +6,35 @@ export default class Parametre {
         this.scene = scene;
         this.canvas = canvas;
 
+        // Div pour l'affichage des fps
         this.divFps = document.getElementById("fps");
+
+        // Ajout de listener pour vérrouiller la souris dans le jeu
+        this.lockPointer();
+    }
+
+    // Actualise l'affichage des fps
+    showFps(fps) {
+        this.divFps.innerHTML = fps + " fps";
     }
 
 
-    showFps(fps) {
-        this.divFps.innerHTML = fps + " fps";
+    // Ajout de listener pour vérrouiller la souris dans le jeu
+    lockPointer() {
+        this.scene.onPointerDown = () => {
+            if (!this.scene.alreadyLocked) {
+                this.canvas.requestPointerLock();
+            }
+        }
+
+        document.addEventListener("pointerlockchange", () => {
+            let element = document.pointerLockElement || null;
+            if (element) {
+                this.scene.alreadyLocked = true;
+            }
+            else {
+                this.scene.alreadyLocked = false;
+            }
+        })
     }
 }
