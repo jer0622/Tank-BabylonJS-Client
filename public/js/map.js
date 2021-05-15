733b879664b777366d3ce7c4735d674468cd0d9c
jeremy <jeremy.hirth-daumas@etu.unice.fr>
2021-05-15 19:03:52 +0200

    Ajout d'un ciel

--- public/js/map.js
+++ public/js/map.js
@@ -15,6 +15,18 @@ export async function loadMap(scene) {
 
     scene.getMeshByName("sea").material.needDepthPrePass = true;
     scene.getLightByName("Sun").intensity = 10;
+
+
+    // Skybox
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 5000.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("//www.babylonjs.com/assets/skybox/TropicalSunnyDay", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.disableLighting = true;
+    skybox.material = skyboxMaterial;
     
 
     let light0 = new BABYLON.DirectionalLight("dir0", new BABYLON.Vector3(-1, -1, 0), scene);
